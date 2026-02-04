d35af7afe58e4537da049e0cd7401755b0318f15
pao-th <paolathpth@gmail.com>
2026-02-04 09:25:17 -0600

    Add files via upload

---
+++ pao/main.py
@@ -0,0 +1,13 @@
+from clase5 import*
+
+per1= Nombre("bernardo",18, "ciencias de la compu")
+per2=Nombre("mich",18,"ciencias de la compu")
+per3=Nombre("lupita",18,"cienciia de datos")
+
+print(str(per1))
+print(repr(per2))
+
+print(per1 + per2)
+print(per2*per1)
+print(per1==per2)
+print(per1==per3)
---
+++ pao/ventana.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+ventana=tk.Tk()
+ventana.title("mi primer ventana")
+etiqueta=tk.Label(ventana, text="hola clase")
+etiqueta.pack(pady=20)
+ventana.mainloop()
